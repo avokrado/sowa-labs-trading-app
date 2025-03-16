204fa2faa2e6e0d37ceb08ff522c7a09484dcdd3
Uros <gomilseku@gmail.com>
2025-03-16 20:32:05 +0100

    feat: showing portfolio in header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,9 +4,12 @@ import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
+import { Provider } from "react-redux";
 
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "@/components/ui/Header";
+import { store } from "@/store";
+import { wsService } from "@/services/websocket";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -22,17 +25,28 @@ export default function RootLayout() {
     }
   }, [loaded]);
 
+  useEffect(() => {
+    // Connect to WebSocket when component mounts
+    wsService.connect();
+
+    // Cleanup WebSocket connection when component unmounts
+    return () => {
+      wsService.disconnect();
+    };
+  }, []);
+
   if (!loaded) {
     return null;
   }
-
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Stack>
-        <Stack.Screen name="index" options={{ header: () => <Header /> }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <Provider store={store}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Stack>
+          <Stack.Screen name="index" options={{ header: () => <Header /> }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </Provider>
   );
 }
--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,19 +1,41 @@
+import { RootState } from "@/store";
+import { useSelector } from "react-redux";
 import React from "react";
 import { View, Text } from "react-native";
 
 function Header() {
+  const currentBalance = useSelector(
+    (state: RootState) => state.portfolio.btcBalance
+  );
+
+  const currentPrice = useSelector(
+    (state: RootState) => state.price.currentPrice
+  );
+
   return (
     <View
       style={{
+        padding: 16,
+        backgroundColor: "#fff",
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
-        padding: 16,
-        backgroundColor: "#fff",
       }}
     >
       <Text>Logo</Text>
-      <Text>Price</Text>
+      <View style={{ gap: 4, alignItems: "flex-end" }}>
+        <Text>Available</Text>
+        <Text>
+          {currentBalance} <Text style={{ fontWeight: "semibold" }}>BTC</Text>
+        </Text>
+        <Text>
+          {(currentBalance * currentPrice).toLocaleString("de-DE", {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          })}{" "}
+          <Text style={{ fontWeight: "semibold" }}>EUR</Text>
+        </Text>
+      </View>
     </View>
   );
 }
