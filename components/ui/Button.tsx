a668681886405b26e5f79ffd35f9b192abaad049
Uros <gomilseku@gmail.com>
2025-03-19 15:56:02 +0100

    feat: improved button component

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,42 +1,75 @@
-import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { TouchableOpacity, Text, StyleSheet, View } from "react-native";
 
 interface ButtonProps {
   onPress: () => void;
   title: string;
-  style?: object;
   disabled?: boolean;
+  error?: string;
 }
 
 export default function Button({
   onPress,
   title,
-  style,
   disabled,
+  error,
 }: ButtonProps) {
   return (
-    <TouchableOpacity
-      style={[styles.button, style]}
-      onPress={onPress}
-      disabled={disabled}
-    >
-      <Text style={styles.text}>{title}</Text>
-    </TouchableOpacity>
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={[
+          styles.button,
+          disabled && styles.disabledButton,
+          error && styles.errorButton,
+        ]}
+        onPress={onPress}
+        disabled={disabled}
+      >
+        <Text
+          style={[
+            styles.text,
+            disabled && styles.disabledText,
+            error && styles.errorText,
+          ]}
+        >
+          {title}
+        </Text>
+      </TouchableOpacity>
+      {error && <Text style={styles.errorText}>{error}</Text>}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 8,
+  },
   button: {
     backgroundColor: "#272947",
     paddingHorizontal: 20,
     paddingVertical: 22,
     borderRadius: 8,
-    flex: 1,
-    flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
   },
   text: {
     color: "white",
     fontSize: 16,
+    textAlign: "center",
+  },
+  disabledButton: {
+    backgroundColor: "#27294750",
+  },
+  disabledText: {
+    color: "#ffffff80",
+  },
+  errorButton: {
+    backgroundColor: "#ff000020",
+    borderWidth: 1,
+    borderColor: "red",
+  },
+  errorText: {
+    color: "red",
+    textAlign: "center",
   },
 });
