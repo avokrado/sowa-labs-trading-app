0d7a35ea3411d156c92fb6a0e3aacbbdfde39eab
Uros <gomilseku@gmail.com>
2025-03-19 15:58:53 +0100

    chore: improved trade modal

--- components/ui/TradeModal.tsx
+++ components/ui/TradeModal.tsx
@@ -1,150 +1,125 @@
+import React from "react";
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
   TextInput,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import Modal from "react-native-modal";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/store";
 import { closeModal } from "@/store/modalSlice";
-import { buyBitcoin, sellBitcoin } from "@/store/portfolioSlice";
-import { useEffect, useState } from "react";
 import Button from "./Button";
+import { useTradeForm } from "@/hooks/useTradeForm";
+import { useTrade } from "@/hooks/useTrade";
 
-const DEFAULT_EUR_AMOUNT = 100;
 export default function TradeModal() {
   const dispatch = useDispatch();
   const isOpen = useSelector((state: RootState) => state.modal.isOpen);
-  const currentPrice = useSelector(
-    (state: RootState) => state.price.currentPrice
-  );
+  const wsStatus = useSelector((state: RootState) => state.price.wsStatus);
+  const {
+    eurAmount,
+    btcAmount,
+    currentPrice,
+    handleEurChange,
+    handleBtcChange,
+    resetForm,
+  } = useTradeForm();
 
-  const [eurAmount, setEurAmount] = useState<number | null>(DEFAULT_EUR_AMOUNT);
-  const [btcAmount, setBtcAmount] = useState<string | null>(null);
+  const { buyError, sellError, resetErrors, handleBuy, handleSell } =
+    useTrade();
 
-  useEffect(() => {
-    if (currentPrice) {
-      setBtcAmount((DEFAULT_EUR_AMOUNT / currentPrice).toFixed(8));
-    }
-  }, [currentPrice]);
-
-  const handleBuy = () => {
-    if (!eurAmount) return;
-    dispatch(buyBitcoin({ amountInEUR: eurAmount, currentPrice }));
+  function handleCloseModal() {
     dispatch(closeModal());
-    setEurAmount(null);
-    setBtcAmount(null);
+    resetErrors();
+    resetForm();
+  }
+
+  const executeBuy = () => {
+    const eurVal = parseFloat(eurAmount);
+    const btcVal = parseFloat(btcAmount);
+    handleBuy({ eurVal, btcVal, currentPrice, onSuccess: handleCloseModal });
   };
 
-  const handleSell = () => {
-    if (!btcAmount) return;
-    dispatch(sellBitcoin({ amountInBTC: btcAmount, currentPrice }));
-    dispatch(closeModal());
-    setEurAmount(null);
-    setBtcAmount(null);
-  };
-
-  const handleEurChange = (text: string) => {
-    const newEurAmount = text ? parseFloat(text) : null;
-    setEurAmount(newEurAmount);
-    setBtcAmount(
-      newEurAmount && currentPrice
-        ? (newEurAmount / currentPrice).toFixed(8).toString()
-        : null
-    );
-  };
-
-  const handleBtcChange = (text: string) => {
-    setBtcAmount(text || null);
-    setEurAmount(text && currentPrice ? parseFloat(text) * currentPrice : null);
+  const executeSell = () => {
+    const eurVal = parseFloat(eurAmount);
+    const btcVal = parseFloat(btcAmount);
+    handleSell({ eurVal, btcVal, currentPrice, onSuccess: handleCloseModal });
   };
 
   return (
     <Modal
       isVisible={isOpen}
-      onBackdropPress={() => dispatch(closeModal())}
-      onBackButtonPress={() => dispatch(closeModal())}
+      onBackdropPress={handleCloseModal}
+      onBackButtonPress={handleCloseModal}
       useNativeDriver
       hideModalContentWhileAnimating
       backdropOpacity={0.5}
       style={styles.modal}
     >
-      <View style={styles.modalView}>
-        <TouchableOpacity
-          onPress={() => dispatch(closeModal())}
-          style={{ alignSelf: "flex-end" }}
-        >
-          <Text style={{ fontWeight: "semibold", fontSize: 24 }}>x</Text>
-        </TouchableOpacity>
-        <View
-          style={{
-            flexDirection: "row",
-            gap: 16,
-            borderColor: "gray",
-            borderRadius: 10,
-            paddingHorizontal: 16,
-            paddingVertical: 16,
-            backgroundColor: "#f3f4f6",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <TextInput
-            value={eurAmount?.toString()}
-            onChangeText={handleEurChange}
-            textAlign="right"
-            keyboardType="numeric"
-            style={{
-              borderBottomWidth: 1,
-              paddingHorizontal: 16,
-              paddingVertical: 16,
-              flex: 1,
-              backgroundColor: "white",
-              color: "black",
-            }}
-          />
-          <Text style={{ fontWeight: "semibold", fontSize: 16, color: "blue" }}>
-            EUR
-          </Text>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+      >
+        <View style={styles.modalView}>
+          <TouchableOpacity
+            onPress={handleCloseModal}
+            style={{ alignSelf: "flex-end" }}
+          >
+            <Text style={{ fontWeight: "semibold", fontSize: 24 }}>x</Text>
+          </TouchableOpacity>
+
+          {/* EUR Input */}
+          <View style={styles.inputRow}>
+            <TextInput
+              value={eurAmount}
+              onChangeText={handleEurChange}
+              // Use decimal-pad on iOS, numeric on Android
+              keyboardType={
+                Platform.select({
+                  ios: "decimal-pad",
+                  android: "numeric",
+                }) || "numeric"
+              }
+              style={styles.input}
+            />
+            <Text style={styles.label}>EUR</Text>
+          </View>
+
+          {/* BTC Input */}
+          <View style={styles.inputRow}>
+            <TextInput
+              value={btcAmount}
+              onChangeText={handleBtcChange}
+              keyboardType={
+                Platform.select({
+                  ios: "decimal-pad",
+                  android: "numeric",
+                }) || "numeric"
+              }
+              style={styles.input}
+            />
+            <Text style={styles.label}>BTC</Text>
+          </View>
+
+          <View style={{ flexDirection: "row", gap: 16 }}>
+            <Button
+              onPress={executeBuy}
+              title="Buy"
+              error={buyError}
+              disabled={wsStatus !== "connected" || !eurAmount || !btcAmount}
+            />
+            <Button
+              onPress={executeSell}
+              title="Sell"
+              error={sellError}
+              disabled={wsStatus !== "connected" || !eurAmount || !btcAmount}
+            />
+          </View>
         </View>
-        <View
-          style={{
-            flexDirection: "row",
-            gap: 16,
-            borderColor: "gray",
-            borderRadius: 10,
-            paddingHorizontal: 16,
-            paddingVertical: 16,
-            backgroundColor: "#f3f4f6",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <TextInput
-            value={btcAmount?.toString()}
-            onChangeText={handleBtcChange}
-            textAlign="right"
-            keyboardType="numeric"
-            style={{
-              borderBottomWidth: 1,
-              paddingHorizontal: 16,
-              paddingVertical: 16,
-              flex: 1,
-              backgroundColor: "white",
-              color: "black",
-            }}
-          />
-          <Text style={{ fontWeight: "semibold", fontSize: 16, color: "blue" }}>
-            BTC
-          </Text>
-        </View>
-        <View style={{ flexDirection: "row", gap: 16 }}>
-          <Button onPress={handleBuy} title="Buy" />
-          <Button onPress={handleSell} title="Sell" />
-        </View>
-      </View>
+      </KeyboardAvoidingView>
     </Modal>
   );
 }
@@ -163,21 +138,30 @@ const styles = StyleSheet.create({
     paddingHorizontal: 32,
     paddingVertical: 16,
     alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
   },
-  header: {
-    flex: 1,
-    width: "100%",
+  inputRow: {
     flexDirection: "row",
+    gap: 16,
+    borderColor: "gray",
+    borderRadius: 10,
+    paddingHorizontal: 16,
+    paddingVertical: 16,
+    backgroundColor: "#f3f4f6",
     justifyContent: "space-between",
-    alignItems: "flex-end",
-    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  input: {
+    borderBottomWidth: 1,
+    paddingHorizontal: 16,
+    paddingVertical: 16,
+    flex: 1,
+    backgroundColor: "white",
+    color: "black",
+    textAlign: "right",
+  },
+  label: {
+    fontWeight: "semibold",
+    fontSize: 16,
+    color: "blue",
   },
 });
